5a0fb319c3f5270256d7ae5ab1281e0b4cd465c2
Ryan Shipley <ryanshipley@Ryans-MacBook-Pro.local>
2022-08-30 11:10:55 -0400

    sets up database

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const drinks = require("./models/drinks.js");
 
 app.get("/", (req, res)=>{
     res.send("Welcome to the Gitpub app!");
+});
+
+app.get("/drinks", (req, res)=>{
+    res.send(drinks);
 });
 
 app.listen(3000, ()=>{
