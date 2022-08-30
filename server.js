3ec8f4d6789f316e3d9fe326667e80777042222b
Ryan Shipley <ryanshipley@Ryans-MacBook-Pro.local>
2022-08-30 13:13:43 -0400

    renders individual drinks

--- server.js
+++ server.js
@@ -14,7 +14,9 @@ app.get("/drinks", (req, res)=>{
 });
 
 app.get("/drinks/:id", (req, res)=>{
-    res.send(req.params.id);
+    res.render("drinks_show.ejs", {
+        drink : drinks[req.params.id],
+    });
 })
 
 app.listen(3000, ()=>{
